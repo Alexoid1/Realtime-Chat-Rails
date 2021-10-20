bacc5e3e95586bbda0ffd329a6ea8cb509501352
alex <alex_zam@hotmail.es>
2021-10-19 21:03:09 -0500

    add more stats to users interface

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -8,11 +8,14 @@ class UserDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
+    
     notifications: Field::HasMany,
     services: Field::HasMany,
+    channel_users: Field::HasMany,
+    channels: Field::HasMany,
+    messages: Field::HasMany,
     id: Field::Number,
     email: Field::String,
-    password: Field::String.with_options(searchable: false),
     encrypted_password: Field::String,
     reset_password_token: Field::String,
     reset_password_sent_at: Field::DateTime,
@@ -23,6 +26,8 @@ class UserDashboard < Administrate::BaseDashboard
     admin: Field::Boolean,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
+    username: Field::String,
+    
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -31,17 +36,23 @@ class UserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  notifications
-  services
-  id
-  email
-  ].freeze
+    notifications
+    services
+    id
+    email
+    channel_users
+    messages
+    ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
+ 
   notifications
   services
+  channel_users
+  channels
+  messages
   id
   email
   encrypted_password
@@ -54,14 +65,20 @@ class UserDashboard < Administrate::BaseDashboard
   admin
   created_at
   updated_at
+  username
+  
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
+  
   notifications
   services
+  channel_users
+  channels
+  messages
   email
   encrypted_password
   reset_password_token
@@ -71,6 +88,8 @@ class UserDashboard < Administrate::BaseDashboard
   last_name
   announcements_last_read_at
   admin
+  username
+  
   ].freeze
 
   # COLLECTION_FILTERS
