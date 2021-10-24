b8e01e1d7095480f50ef779854bb6a527db9f24e
alex <alex_zam@hotmail.es>
2021-10-23 22:24:15 -0500

    setip rspec and update form sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,9 +3,9 @@ class RegistrationsController < Devise::RegistrationsController
   
     private
     def sign_up_params
-        params.require(:user).permit(:name, :email, :avatar, :password)
+        params.require(:user).permit(:name, :email, :avatar, :password, :password_confirmation, :username)
     end
     def account_update_params
-        params.require(:user).permit(:name, :email, :avatar, :password)
+        params.require(:user).permit(:name, :email, :avatar, :password, :password_confirmation, :username)
     end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-    
+    let(:user_2) { User.create(name: 'Rebecca Y', email: 'recca@opal.com', password: "aaabbb", password_confirmation: "aaabbb", username: "Alexoid") }
+    let(:user_3) { User.create(name: 'Rebecca ryrty', email: 'reccafghjfgopal.com') }
+    subject { User.create(username: 'Ale', fullname: 'a.azzz@overpower.com') }
 end    
