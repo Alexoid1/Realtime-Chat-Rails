1e527dc5ad6f75e7bd76f99c046da6b1ff7de663
alex <alex_zam@hotmail.es>
2021-10-26 00:45:21 -0500

    add messages test

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -5,7 +5,8 @@ class MessageTest < ActiveSupport::TestCase
   def setup  
     @channelTest = Channel.create(name: 'JavaScript')
     @userTest = User.create(name: 'Rebehjkccia y', email: 'recca@opjhkdefal.cokima', password: "vaabbb", username: "fAlexoid")
-    @message = Message.create(user_id: @userTest.id, channel_id: @channelTest.id, body: 'Hello worldddd')
+    @user = User.create(name: 'Rebehjkccia y', email: 'recca@opjhkdefal.cok', password: "vaabbb", username: "Alex")
+    @message = Message.create(user_id: @userTest.id, channel_id: @channelTest.id, body: 'Hello worldddd @fAlexoid')
   end
 
   test "validate message create" do
@@ -19,5 +20,17 @@ class MessageTest < ActiveSupport::TestCase
   test "should belongs to channel" do
     assert @message.channel
   end
+
+  test "test message method mentioned_usernames" do
+    names=@message.mentioned_usernames
+    assert names.length >=1
+  end
+
+  test "test message method mentioned_usernames two usernames" do
+    mes = Message.new(user_id: @userTest.id, channel_id: @channelTest.id, body: 'Hello worldddd @fAlexoid @Alex')
+    name=mes.mentioned_usernames
+    assert name.length == 2
+  end  
+
 
 end
