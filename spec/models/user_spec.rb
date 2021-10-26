11d360b1aad5c50992ff387f1517274f350dd6fa
alex <alex_zam@hotmail.es>
2021-10-25 20:51:24 -0500

    add user test association messagesa chanel

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,24 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-    let(:user_2) { User.create(name: 'Rebecca Y', email: 'recca@opal.com', password: "aaabbb", password_confirmation: "aaabbb", username: "Alexoid") }
-    let(:user_3) { User.create(name: 'Rebecca ryrty', email: 'reccafghjfgopal.com') }
-    subject { User.create(username: 'Ale', fullname: 'a.azzz@overpower.com') }
+    let(:user_2) { User.create(name: 'Rebecca y', email: 'recca@opal.coma', password: "aaabbb", username: "Alexoid") }
+    let(:channel_2) { Channel.create(name: 'JavaScript') }
+    let(:user) { User.create(name: 'Ale am', email: 'aaaaaa@opal.coma', password: "aaabbb", username: "Alexdfh" )}
+    
+    before do
+        ChannelUser.create(user_id: user.id, channel_id: channel_2.id)
+    end
+    context 'Associations' do
+        it { expect(user).to have_many(:notifications) }
+        it { expect(user).to have_many(:messages) }
+        it { expect(user).to have_many(:services) }
+        it { expect(user).to have_many(:channels) }
+        it { expect(user).to have_many(:channel_users) }
+    end
+    after do 
+        user_2.delete
+        
+    end
+
+    
 end    
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ 
+  test "validate user create" do
+    user = User.new(name: 'Rebehjkccia y', email: 'recca@opjhkdefal.cokima', password: "vaabbb", username: "fAlexoid")
+    assert user.valid?
+  end
+
+  test "validate user messages association" do
+    user = User.new(name: 'Rebehjkccia y', email: 'recca@opjhkdefal.cokima', password: "vaabbb", username: "fAlexoid")
+    assert user.messages
+  end
+
+  test "validate user channels association" do
+    user = User.new(name: 'Rebehjkccia y', email: 'recca@opjhkdefal.cokima', password: "vaabbb", username: "fAlexoid")
+    assert  user.channels
+  end  
+
+  test "Should not save user without email" do
+    user = User.new(name: 'Rebehjkccia y', email: '', password: "vaabbb", username: "fAlexoid")
+    assert_not  user.save
+  end 
 end
+
