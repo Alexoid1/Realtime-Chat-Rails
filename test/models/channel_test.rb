6054a0be2360e38e8db44989e818ce0d209d7deb
alex <alex_zam@hotmail.es>
2021-10-25 22:34:47 -0500

    add model channel test

--- test/models/channel_test.rb
+++ test/models/channel_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ChannelTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup  
+    @channelTest = Channel.create(name: 'JavaScript')
+  end
+
+  test "validate channel create" do
+    assert @channelTest.valid?
+  end
+
+  test "validate user-channels association" do
+    assert  @channelTest.users
+  end
+
+  test "validate channels_users association" do
+    assert  @channelTest.channel_users
+  end
+
+  test "validate channels-messages association" do
+    assert  @channelTest.messages
+  end
+  
+  test "validate presence of channels name" do
+    channel = Channel.new(name: '')
+    assert_not  channel.valid?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,41 +1,47 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
- 
+  def setup
+    @userTest = User.create(name: 'Rebehjkccia y', email: 'recca@opjhkdefal.cokima', password: "vaabbb", username: "fAlexoid")
+  end
   test "validate user create" do
-    user = User.new(name: 'Rebehjkccia y', email: 'recca@opjhkdefal.cokima', password: "vaabbb", username: "fAlexoid")
-    assert user.valid?
+    assert @userTest.valid?
   end
 
-  test "validate user-messages association" do
-    user = User.new(name: 'Rebehjkccia y', email: 'recca@opjhkdefal.cokima', password: "vaabbb", username: "fAlexoid")
-    assert user.messages
+  test "validate user-messages association" do 
+    assert @userTest.messages
   end
 
   test "validate user-channels association" do
-    user = User.new(name: 'Rebehjkccia y', email: 'recca@opjhkdefal.cokima', password: "vaabbb", username: "fAlexoid")
-    assert  user.channels
+    assert  @userTest.channels
   end
 
   test "validate channels_users association" do
-    user = User.new(name: 'Rebehjkccia y', email: 'recca@opjhkdefal.cokima', password: "vaabbb", username: "fAlexoid")
-    assert  user.channel_users
+    assert  @userTest.channel_users
   end
 
   test "validate user-notification association" do
-    user = User.new(name: 'Rebehjkccia y', email: 'recca@opjhkdefal.cokima', password: "vaabbb", username: "fAlexoid")
-    assert  user.notifications
+    assert  @userTest.notifications
   end
 
   test "validate user-services association" do
-    user = User.new(name: 'Rebehjkccia y', email: 'recca@opjhkdefal.cokima', password: "vaabbb", username: "fAlexoid")
-    assert  user.services
+    assert  @userTest.services
   end  
 
   test "Should not save user without email" do
     user = User.new(name: 'Rebehjkccia y', email: '', password: "vaabbb", username: "fAlexoid")
-    assert_not  user.save
+    assert_not user.save
   end
+
+  test "validate uniqueness of user email" do
+    user1 = User.new(name: 'Rebehjkccia y', email: 'recca@opjhkdefal.cokima', password: "vaabbb", username: "sadasd")
+    assert_not user1.valid?
+  end 
+
+  test "validate uniqueness of user username" do
+    user=User.new(name: 'Rebehjkccia y', email: 'recca@asdasd.com', password: "vaabbb", username: "fAlexoid")
+    assert_not user.valid?
+  end  
 
   
 end
