69689b0b2163f80e511668227c547025fe06e17c
alex <alex_zam@hotmail.es>
2021-10-26 00:09:03 -0500

    add message model

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup  
+    @channelTest = Channel.create(name: 'JavaScript')
+    @userTest = User.create(name: 'Rebehjkccia y', email: 'recca@opjhkdefal.cokima', password: "vaabbb", username: "fAlexoid")
+    @message = Message.create(user_id: @userTest.id, channel_id: @channelTest.id, body: 'Hello worldddd')
+  end
+
+  test "validate message create" do
+    assert @message.valid?
+  end
+
+  test "should belongs to user" do
+    assert @message.user
+  end
+
+  test "should belongs to channel" do
+    assert @message.channel
+  end
+
 end
